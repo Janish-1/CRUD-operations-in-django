3701ab537320c2d4bc7f059e837d988e0af383c5
Janish Pancholi <61050306+Janish-1@users.noreply.github.com>
2022-04-28 21:02:25 +0530

    hidden the secret key

--- newproject/settings.py
+++ newproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-588n*=5(gc48l2#*hqvskay2v#pn8_ghy*(-_!%@)%6q+9-!53'
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
